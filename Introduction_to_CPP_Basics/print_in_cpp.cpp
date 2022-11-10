942e8dbc086b83aa999bbbd52b29adf8c4ae1437
mdNaimulHoque <naimulhoq89@gmail.com>
2022-11-10 19:26:23 +0600

    cout

---
+++ Introduction_to_CPP_Basics/print_in_cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
+
+    int a = 100;
+    cout << "The value " << a << " is my favorite" << endl;
+
+    return 0;
+}
